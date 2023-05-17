363071af42acac2c5848112d4c831e12a3388751
Santiag0Vidal <89548742+Santiag0Vidal@users.noreply.github.com>
2023-05-17 13:42:48 -0300

    Update Viaje.php

--- Viaje.php
+++ Viaje.php
@@ -1,20 +1,35 @@
 <?php
 
+include 'Pasajero.php';
+include 'PasajeroVIP.php';
+include 'PasajeroConNecesidades.php'; 
+include 'ResponsableV.php';
+
 class Viaje{
 
     private $codigo;
+
     private $destino;
+
     private $cantidad;
+
     private $pasajeros;
+
     private $responsable;
 
-    public function __construct($codigo,$destino,$cantidad,$pasajeros,$responsable)
+    private $costo;
+
+    private $sumaCosto;
+
+    public function __construct($codigo,$destino,$cantidad,$pasajeros, $responsable, $costo, $sumaCosto)
     {
         $this->codigo= $codigo;
         $this->destino= $destino;
         $this->cantidad= $cantidad;
         $this->pasajeros= $pasajeros;
-        $this ->responsable= $responsable;
+        $this->responsable= $responsable;
+        $this->costo=$costo;
+        $this->sumaCosto=$sumaCosto;
     }
 
     public function getCodigo(){
@@ -32,8 +47,9 @@ class Viaje{
     public function getPasajeros(){
         return $this-> pasajeros;
     }
+
     public function getResponsable(){
-        return $this-> responsable;
+        return $this->responsable;
     }
 
     public function setCodigo($codigo){
@@ -51,41 +67,77 @@ class Viaje{
     public function setPasajeros($pasajeros){
         $this->pasajeros= $pasajeros;
     }
+
     public function setResponsable($responsable){
-        $this->pasajeros= $responsable;
+        $this->responsable= $responsable;
+    }
+
+    public function getCosto()
+    {
+        return $this->costo;
+    }
+
+    public function setCosto($costo)
+    {
+        $this->costo = $costo;
+
+        return $this;
+    }
+
+    public function getSumaCosto()
+    {
+        return $this->sumaCosto;
+    }
+
+    public function setSumaCosto($sumaCosto)
+    {
+        $this->sumaCosto = $sumaCosto;
+
+        return $this;
     }
 
     /**
-     * Agrega un nuevo pasajero al arreglo
-     * @param Array Un arreglo asociativo
-     * @return Boolean Retorna verdadero si queda espacio en el arreglo y retorna falso si no queda espacio en el arreglo
+     * Agrega un pasajero al arreglo de pasajeros si todavia hay espacio
+     * @param Pasajero Un Objeto de tipo Pasajero
+     * @return double Retorna el costo a abonar por el pasajero
      */
 
-    public function agregarPasajero($pasajero){
-        $continuar = false;
-        $indice=count($this->pasajeros);
-        if($this->cantidad > $indice){
+    public function venderPasaje($pasajero){
+        $importe=0;
+        $band = $this->hayPasajesDisponible();
+        if($band){
+            $indice=count($this->pasajeros);
             $this->pasajeros[$indice]= $pasajero;
-            $band =true;
+            $importe= $this->getCosto()*(1+$pasajero->darPorcentajeIncremento());
+            $this->sumaCosto= $this->sumaCosto+$importe;
         }
-        return $continuar;
+        return $importe;
     }
     public function __toString()
     {
-        return "(".$this->codigo.", ".$this->destino.", ".$this->cantidad.", ".($this->getResponsable())->__toString().", \n ".$this->auxToString().")";
+        return "(".$this->codigo.", ".$this->destino.", ".$this->cantidad.", ".($this->getResponsable())->__toString().", ".$this->auxToString().", ".$this->getCosto().", ".$this->getSumaCosto().")";
     }
 
-    public function auxToString(){
-        $cadena="Pasajeros: \n";
+    private function auxToString(){
+        $cadena="[";
         $indice=count($this->pasajeros);
         if($indice==0){
             $cadena = "No hay pasajeros";
         }else{
-            $arreglo=  $this->pasajeros;
+            $arreglo=  $this->getPasajeros();
             for($i=0;$i<$indice;$i++){
-                $cadena= $cadena.($arreglo[$i]-> __toString());
+                $cadena= $cadena.($arreglo[$i]-> __toString()).", ";
             }
+                $cadena= substr($cadena, 0,-2)."]";
         }
         return $cadena;
     }
+
+    public function hayPasajesDisponible(){
+        $hayLugar= count($this->getPasajeros())< $this->getCantidad();
+        return $hayLugar;
+    }
+
 }
+
+?>
